a8813fa228350d08ea0e60b64e20b023a7784439
zhaochunen <120024188+zhaochunen@users.noreply.github.com>
2026-02-06 10:48:24 +0800

    Refactor zubo.py by adding imports and functions

--- zubo.py
+++ zubo.py
@@ -143,11 +143,13 @@ def main():
     with open("zubo_all.txt", "w", encoding="utf-8") as f:
         f.write(f"{current_time}更新,#genre#\n")
         f.write(f"浙江卫视4K,https://play-qukan.cztv.com:443/live/1758879019692345.m3u8\n")
+        f.write(f"中视新闻,http://8.218.84.3:8885/live/LxWcScuQ6WphaELY4DhkKm8SIv7_ZgNIJtNvFurqphvyqxUIU84OjKVCwFxMq9iUyH_fs26-c7nBUTry9_G9WMoiY9eSvYzh6ZwnRGmkQCdsjPrEP66LQen6iWL-fzZsqBTXBHKSfZLUc3BwHZ8Q4_yA7qBHgB0punInWfeNbjrSmjvScFFu0zpuxNErW7EIII4-rSdU1ytKDClCBqmsDn2rc00nFckmk5plk1cMpGnBmnI_y3vRoROfc3NhwO-4ZeKxcw_ZE1BegueTZVshjQ.m3u8?u=1756878715\n")
         f.write('\n'.join(file_contents))
     txt_to_m3u("zubo_all.txt", "zubo_all.m3u")
     print(f"组播地址获取完成")
 
 if __name__ == "__main__":
     main()
+
 
 
